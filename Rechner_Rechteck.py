2c2e32507bee19fbdf1ccab8861da6014c337449
Lucas Grimm <lucas.grimm.2000@web.de>
2025-02-19 11:55:20 +0100

    Rechner Rechteck code in funktion gepackt

--- Rechner_Rechteck.py
+++ Rechner_Rechteck.py
@@ -1,4 +1,13 @@
-zahl1 = float(input("Gib die Länge in cm an: "))
-zahl2 = float(input("Gib die Breite in cm an: "))
-ergebnis = zahl1 * zahl2
-print("Ergebnis:", ergebnis, "Quadratcentimeter")
+def rechner():
+    while True:
+        try:
+            zahl1 = float(input("Gib die Länge in cm an: "))
+            zahl2 = float(input("Gib die Breite in cm an: "))
+            ergebnis = zahl1 * zahl2
+            print("Ergebnis:", ergebnis, "Quadratcentimeter")
+            break
+            
+        except ValueError:
+            print("Fehler: Bitte eine gültige Zahl eingeben.")
+
+rechner()
